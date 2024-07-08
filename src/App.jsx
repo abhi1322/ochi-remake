795af2f7776815900b4f6fa44cc4addbfba557cb
abhi1322 <abhishek@Abhishekkumar>
2024-07-08 21:26:30 +0530

    First commit after landing page

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import LandingPage from "./components/LandingPage";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div className="w-full h-screen text-white">
+      <Navbar />
+      <LandingPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,40 @@
+import { FiArrowUpRight } from "react-icons/fi";
+
+function LandingPage() {
+  return (
+    <div className="w-full h-screen bg-zinc-800 pt-2">
+      <div className="textStructure mt-52 px-20 font-['Bebas_Neue']">
+        {["We Creare", "Eye-Opening", "Presentation"].map((text, i) => (
+          <div key={i} className="maskee">
+            <h1 className="uppercase text-[8vw] leading-[6.2vw] tracking ">
+              {text}
+            </h1>
+          </div>
+        ))}
+      </div>
+      <div className="border-t border-zinc-700 mt-52 flex items-center justify-between pt-10 px-20">
+        {[
+          "For public and private companies",
+          "From the first pitch to IPO",
+        ].map((text, i) => (
+          <p key={i} className="text-lg font-light tracking-tight leading-none">
+            {text}
+          </p>
+        ))}
+        <div className="group start flex gap-1 items-center">
+          <div className="uppercase border rounded-full px-4 py-2 text-sm group-hover:bg-white group-hover:text-zinc-800 cursor-pointer transition-all ease-in-out ">
+            START THE PROJECT
+          </div>
+          <div className="w-9 h-9 border rounded-full flex justify-center items-center group-hover:bg-white">
+            <FiArrowUpRight
+              className="w-full h-full group-hover:text-zinc-800"
+              strokeWidth={1}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default LandingPage;
