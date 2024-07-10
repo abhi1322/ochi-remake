35726b1a5c77693e92d8577a602ddba8b84068bb
abhi1322 <abhi1322jii@gmail.com>
2024-07-10 21:07:08 +0530

    project section done

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -2,6 +2,7 @@ import PropTypes from "prop-types";
 import Button from "./ui/Button";
 import { motion } from "framer-motion";
 import { useState } from "react";
+import { FiChevronRight } from "react-icons/fi";
 
 function Features() {
   const cardsData = [
@@ -26,7 +27,7 @@ function Features() {
       <div className="w-full px-20 pb-10 border-b border-zinc-600">
         <h3 className="text-6xl tracking-tight">Featured Projects</h3>
       </div>
-      <motion.div className="relative cards w-full flex px-20 py-10 gap-10">
+      <motion.div className="relative cards w-full flex px-20 py-10 gap-10 mb-40">
         {isHoveredTitle != "" ? (
           <div className="absolute flex justify-center items-center gap-1 top-[50%] right-[50%] translate-x-[50%] -translate-y-[50%] text-9xl z-50 uppercase font-['Bebas_Neue'] text-[#c0da63] overflow-hidden">
             {isHoveredTitle.split("").map((l, i) => (
@@ -36,10 +37,9 @@ function Features() {
                 transition={{
                   duration: 0.2,
                   ease: "easeOut",
-                  delay: i * 0.11, // Add delay for staggered effect
+                  delay: i * 0.11,
                 }}
                 key={i}
-                // className="translate-y-[100%]"
               >
                 {l}
               </motion.p>
@@ -52,10 +52,22 @@ function Features() {
             title={card.title}
             imgSrc={card.imgSrc}
             tags={card.tags}
-            onHovered={setisHoveredTitle} // Use onHovered prop
+            onHovered={setisHoveredTitle}
           />
         ))}
       </motion.div>
+
+      <div className="w-full flex items-center justify-center ">
+        <button className="group bg-white group-hover:bg-neutral-200 text-zinc-800 text-xl tracking-tighter uppercase rounded-full px-8 py-4 flex justify-between items-center gap-6 fo ">
+          checkout more case studies
+          <span className=" bg-zinc-800 rounded-full group-hover:scale-150 flex items-center justify-center scale-50">
+            <FiChevronRight
+              className="group-hover:scale-['100%']  scale-0 text-white transition-all ease-in duration-100"
+              strokeWidth={1}
+            />
+          </span>
+        </button>
+      </div>
     </div>
   );
 }
@@ -66,14 +78,14 @@ function Card({ title, imgSrc, tags, onHovered }) {
     title: PropTypes.string.isRequired,
     imgSrc: PropTypes.string.isRequired,
     tags: PropTypes.arrayOf(PropTypes.string).isRequired,
-    onHovered: PropTypes.func.isRequired, // Correct prop type
+    onHovered: PropTypes.func.isRequired,
   };
 
   return (
     <motion.div
       className="card w-1/2 h-[60vh]"
-      onMouseEnter={() => onHovered(title)} // Use onHovered here
-      onMouseLeave={() => onHovered("")} // Use onHovered here
+      onMouseEnter={() => onHovered(title)}
+      onMouseLeave={() => onHovered("")}
     >
       <div className="title flex gap-2 items-center mb-4">
         <div className="h-2 w-2 bg-white rounded-full"></div>
